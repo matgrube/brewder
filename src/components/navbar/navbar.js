aac77a998a1d56f7de40b49fa22cffeb0987d383
MatGrube <m.grube@outlook.com>
2020-01-29 23:46:13 +0100

    Adding component to App

---
+++ src/components/navbar/navbar.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const navigation = () => {
+    return (
+        <div className="navbar_top">
+            <ul>
+                <li>App</li>
+                <li>About</li>
+                <li>Contact</li>
+            </ul>
+        </div>
+    )
+}
+
+export default navigation;
