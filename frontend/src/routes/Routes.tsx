5cd7abdc5c8a464c2a85c75ebd4518f6f661ebc0
Mateusz Grube <m.grube@outlook.com>
2022-01-25 23:00:37 +0100

    Added basic Router and first loginPage

--- frontend/src/routes/Routes.tsx
+++ frontend/src/routes/Routes.tsx
@@ -1,9 +1,14 @@
 import React from "react";
+import {BrowserRouter, Routes as ReactRoutes, Route} from "react-router-dom";
+import {LoginPage} from "../pages/loginPage/LoginPage";
 //import { Switch } from "react-router-dom";
 
 export const Routes: React.FC = () => {
   return (
-    <>
-    </>
+    <BrowserRouter>
+      <ReactRoutes>
+        <Route path="*" element={<LoginPage />} />
+      </ReactRoutes>
+    </BrowserRouter>
   )
 }
