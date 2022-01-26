2cd15765ae4b04d37a239cad11e367de55da39f1
matgrube <47569899+matgrube@users.noreply.github.com>
2022-01-26 23:36:28 +0100

    Add routing (#1)
    * Added basic Router and first loginPage
    
    * imports cleanup

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Routes } from "./routes/Routes";
 import {Router} from 'react-router';
 
 function App() {
   return (
     <div className="App">
+     <Routes />
     </div>
   );
 }
--- frontend/src/routes/Routes.tsx
+++ frontend/src/routes/Routes.tsx
@@ -1,9 +1,13 @@
 import React from "react";
-//import { Switch } from "react-router-dom";
+import {BrowserRouter, Routes as ReactRoutes, Route} from "react-router-dom";
+import {LoginPage} from "../pages/loginPage/LoginPage";
 
 export const Routes: React.FC = () => {
   return (
-    <>
-    </>
+    <BrowserRouter>
+      <ReactRoutes>
+        <Route path="*" element={<LoginPage />} />
+      </ReactRoutes>
+    </BrowserRouter>
   )
 }
