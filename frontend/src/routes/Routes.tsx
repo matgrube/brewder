8e21dacca78105bbe020a6a491c195646b99196d
Mateusz Grube <m.grube@outlook.com>
2022-01-26 23:35:22 +0100

    imports cleanup

--- frontend/src/routes/Routes.tsx
+++ frontend/src/routes/Routes.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import {BrowserRouter, Routes as ReactRoutes, Route} from "react-router-dom";
 import {LoginPage} from "../pages/loginPage/LoginPage";
-//import { Switch } from "react-router-dom";
 
 export const Routes: React.FC = () => {
   return (
